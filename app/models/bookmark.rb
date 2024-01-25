f0558e30d5ab6b10984e337f5797378628f607f3
luisa santos <1analuisantos@gmail.com>
2024-01-25 11:30:41 +0000

    Rails Watch List

---
+++ app/models/bookmark.rb
@@ -0,0 +1,7 @@
+class Bookmark < ApplicationRecord
+  validates :comment, length: { minimum: 6 }
+  validates_uniqueness_of :movie_id, scope: :list_id
+
+  belongs_to :movie
+  belongs_to :list
+end
