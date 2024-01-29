2ab6638766c6dfd7375e22c70b622ea72e5e3674
luisa santos <1analuisantos@gmail.com>
2024-01-29 12:10:10 +0000

    Rails Watch List

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,35 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-List.destroy_all if Rails.env.development?
+require 'open-uri'
+require 'json'
 
-movies_data = [
-  {
-    title: 'City of God',
-    overview: 'In the slums of Rio, two kids paths diverge as one struggles to become a photographer and the other a kingpin.',
-    poster_url: 'https://www.themoviedb.org/t/p/w1280/k7eYdWvhYQyRQoU2TB2A2Xu2TfD.jpg',
-    rating: 8.4
-  },
-  {
-    title: 'The Shawshank Redemption',
-    overview: 'Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison',
-    poster_url: 'https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg',
-    rating: 8.7
-  },
-  {
-    title: 'Titanic',
-    overview: '101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.',
-    poster_url: 'https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg',
-    rating: 7.9
-  },
-  {
-    title: 'Oceans Eight',
-    overview: 'Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.',
-    poster_url: 'https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg',
-    rating: 7.0
-  }
-]
+puts "Cleaning up database..."
+Movie.destroy_all
+puts "Database cleaned"
 
-movies_data.each do |movie_data|
-  Movie.create(movie_data)
+url = "http://tmdb.lewagon.com/movie/top_rated"
+10.times do |i|
+  puts "Importing movies from page #{i + 1}"
+  movies = JSON.parse(URI.open("#{url}?page=#{i + 1}").read)['results']
+  movies.each do |movie|
+    puts "Creating #{movie['title']}"
+    base_poster_url = "https://image.tmdb.org/t/p/original"
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+      rating: movie['vote_average']
+    )
+  end
 end
+puts "Movies created"
